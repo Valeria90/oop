4834bca3bb67d0e845b4019ff3e4fd211931419b
Valeria <valeria.shevchenko.90@gmail.com>
2018-05-07 11:14:31 +0300

    add 7 task

--- script.js
+++ script.js
@@ -113,3 +113,104 @@ let count = counter();
 alert(count());
 alert(count());
 alert(count());
+
+class List {
+    constructor(options) {
+        this.ctrl = options.ctrl;
+        this.parentElement = options.parentElement;
+        this.itemElement = options.itemElement;
+        this.arrOfItems = options.arrOfItems;
+        this.onCreate = options.onCreate;
+        this.onDelete = options.onDelete;
+        this.checkedList = [];
+    }
+    createStartPage() {
+        let body = document.querySelector('body');
+        body.innerHTML = "<input><button></button><button></button><ul></ul>";
+        let getDataInput = document.querySelector('input');
+        let buttonAdd = document.querySelectorAll('button')[0];
+        buttonAdd.innerText = '+';
+        let buttonDel = document.querySelectorAll('button')[1];
+        buttonDel.innerText = '-';
+        buttonDel.classList.add('hide');
+        let list = document.querySelector(this.parentElement);
+        for(let i = 0; i < 5; i++) {
+            let li = document.createElement(this.itemElement);
+            let innerI = document.createTextNode(this.arrOfItems[i]);
+            li.appendChild(innerI);
+            list.appendChild(li);
+        }
+        buttonAdd.addEventListener('click', this.onCreate);
+        buttonDel.addEventListener('click', this.onDelete.bind(this));
+        list.addEventListener('click', this.check.bind(this));
+    }
+    check(event) {
+        let buttonDel = document.querySelectorAll('button')[1];
+        let itemList = document.querySelectorAll('li');
+        let checkedItem = event.target;
+        if(event.ctrlKey){
+            if(!checkedItem.classList.contains('checked')) {
+                checkedItem.classList.add('checked');
+                this.checkedList.push(checkedItem);
+            } else {
+                checkedItem.classList.remove('checked');
+                for(let i = 0; i < this.checkedList.length; i++) {
+                    if (this.checkedList[i] == checkedItem) {
+                        this.checkedList.splice(this.checkedList[i]);
+                    }
+                } 
+            }
+        } else {
+            if(!checkedItem.classList.contains('checked')) {
+                for(let i = 0; i < itemList.length; i++) {
+                    itemList[i].classList.remove('checked');
+                }
+                checkedItem.classList.add('checked');
+                this.checkedList.push(checkedItem);
+            } else {
+                if(this.checkedList.length > 1) {
+                    for(let i = 0; i < itemList.length; i++) {
+                        itemList[i].classList.remove('checked');
+                    }
+                    this.checkedList = [];
+                    checkedItem.classList.add('checked');
+                    this.checkedList.push(checkedItem); 
+                } else {
+                    for(let i = 0; i < itemList.length; i++) {
+                        itemList[i].classList.remove('checked');
+                    }
+                    this.checkedList = [];
+                }
+            }
+        }
+        if(this.checkedList.length > 0) {
+            buttonDel.classList.remove('hide');
+            buttonDel.classList.add('show');
+        } else {
+            buttonDel.classList.add('hide');
+            buttonDel.classList.remove('show');
+        }
+    }
+}
+function addListElement() {
+    let list = document.querySelector('ul');
+    let input = document.querySelector('input');
+    let li = document.createElement('li');
+    let addTextToNewItem = document.createTextNode(input.value);
+    li.appendChild(addTextToNewItem);
+    list.appendChild(li);
+    input.value = '';
+}
+function removeListElement() { 
+    for(let i = 0; i < this.checkedList.length; i++) {
+        this.checkedList[i].remove();
+    }
+}
+let item = new List({
+    ctrl: true, 
+    parentElement: 'ul', 
+    itemElement: 'li', 
+    arrOfItems: ['1', '2', '3', '4', '5'], 
+    onCreate: addListElement, 
+    onDelete: removeListElement});
+item.createStartPage();
